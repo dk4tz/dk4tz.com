79fc557d643830c7176ffd26cf7cc8c3ff0106d8
dkatz <hellodavidkatz@pm.me>
2023-10-04 21:14:08 -0400

    clean up renaming

--- faster-client/vite.config.ts
+++ faster-client/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/dk4tz.com/',
-  assetsInclude: ['**/*.glb'],
+	plugins: [react()],
+	base: '/portrait/',
+	assetsInclude: ['**/*.glb']
 });
